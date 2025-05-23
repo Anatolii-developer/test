34920cc50262e2e0d72f096ecacab0b0f95e4a80
Anatolii-developer <113465994+Anatolii-developer@users.noreply.github.com>
2025-05-23 11:06:40 -0400

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,5 +6,6 @@ router.post('/register', registerUser);
 router.get('/', getAllUsers); // получить всех
 router.get('/:id', getUserById); // получить одного
 router.put('/:id/status', updateUserStatus); // обновить статус
+router.post("/login", loginUser);
 
 module.exports = router;

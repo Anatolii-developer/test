777bdb5e2107c7e2838a157c4dd5bd4a5990b0fd
Anatolii-developer <113465994+Anatolii-developer@users.noreply.github.com>
2025-06-18 15:09:30 -0400

    Update courseController.js

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -5,7 +5,8 @@ exports.createCourse = async (req, res) => {
   try {
     const course = new Course(req.body);
     await course.save();
-    res.status(201).json({ message: "Курс створено", course });
+res.status(201).json({ success: true, message: "Курс створено", course });
+
  } catch (error) {
   console.error("❌ Error creating course:", error.message, error.stack);
   res.status(500).json({ message: "Помилка при створенні курсу", error: error.message });

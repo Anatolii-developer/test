a683b989f8207377c4f640605e05c5882c75f160
Anatolii-developer <113465994+Anatolii-developer@users.noreply.github.com>
2025-06-24 11:28:53 -0400

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const { registerUser, getAllUsers, getUserById, updateUserStatus, loginUser, updateUser } = require('../controllers/userController');
+const { sendRecoveryCode } = require('../controllers/userController');
+const { registerUser, getAllUsers, getUserById, updateUserStatus, loginUser, updateUser, sendRecoveryCode} = require('../controllers/userController');
 const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const { uploadUserPhoto } = require('../controllers/userController');
 
 router.post('/register', registerUser);
 router.get('/', getAllUsers);
 router.post("/login", loginUser);
 router.put('/:id/status', updateUserStatus); // более специфичный
 router.get('/:id', getUserById);             // общий get
-router.put('/:id', updateUser);              // общий put
+router.put('/:id', updateUser);  
+router.post("/forgot-password", sendRecoveryCode);
+router.post("/:id/photo", upload.single("photo"), uploadUserPhoto);
+            // общий put
+
 
 module.exports = router;

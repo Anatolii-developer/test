0c407958fbc796c5fdc23949a8a00f1323a3c58b
Anatolii-developer <113465994+Anatolii-developer@users.noreply.github.com>
2025-07-01 10:36:52 -0400

    Update courseRoutes.js

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createCourse, getCourses, getCourseById } = require('../controllers/courseController');
+const { createCourse, getCourses, getCourseById, updateCourse } = require('../controllers/courseController');
 
-
-router.post('/', createCourse);   // ✅ просто /
-router.get('/', getCourses);      // ✅ просто /
+router.post('/', createCourse);
+router.get('/', getCourses);
 router.get('/:id', getCourseById);
-
+router.put('/:id', updateCourse);
 
 module.exports = router;

30e5048412c6b8c893c305708f8a2a46084eea5c
Anatolii-developer <113465994+Anatolii-developer@users.noreply.github.com>
2025-07-01 10:13:14 -0400

    Update Course.js

--- backend/models/Course.js
+++ backend/models/Course.js
@@ -8,23 +8,22 @@ const courseSchema = new mongoose.Schema({
   courseTitle: String,
   courseSubtitle: String,
   courseDescription: String,
-courseDates: {
-  start: new Date(form.startDate.value),
-  end: new Date(form.endDate.value)
-},
+  courseDates: {
+    start: Date,
+    end: Date
+  },
   courseDays: [String], // ['Понеділок', 'Середа', ...]
   courseTime: {
-    start: String, // e.g. "17:00"
-    end: String    // e.g. "18:00"
+    start: String,
+    end: String
   },
   accessType: String, // Відкрита група / Закрита група
-  closedGroupMembers: [String], // optional user IDs
+  closedGroupMembers: [String],
   participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   courseDuration: String,
   coursePrice: String,
   zoomLink: String,
   createdAt: { type: Date, default: Date.now }
-  
 });
 
 module.exports = mongoose.model('Course', courseSchema);

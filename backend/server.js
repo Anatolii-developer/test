d4673d4a46f2468deae178f634e995f339771e3b
Anatolii Hryhorzhevskyi <kavo@Anatoliis-MacBook-Pro.local>
2025-07-11 11:57:31 -0400

    Добавлены последние изменения

--- backend/server.js
+++ backend/server.js
@@ -9,33 +9,23 @@ dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();  
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
 // ✅ Статические файлы
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // ✅ Роуты
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
-const uploadRoutes = require('./routes/uploadRoutes');
-app.use(uploadRoutes); // без префикса, потому что маршрут уже включает /api/users
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/uploads", express.static("uploads"));
-
+const certificateRoutes = require('./routes/certificateRoutes');
+app.use('/api/users', certificateRoutes); // префикс добавлен один раз — правильно
 
 const courseRoutes = require('./routes/courseRoutes');
 app.use('/api/courses', courseRoutes);
-
-const certificateRoutes = require('./routes/certificateRoutes');
-app.use('/api/users', certificateRoutes); // ✅ Добавляем общий префикс
-
-
-
-
 
 // ✅ MongoDB
 mongoose.connect(process.env.MONGO_URI)

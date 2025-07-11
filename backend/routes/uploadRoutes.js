b7744fab2ac977b58f0651483734e6021e8d5034
Anatolii Hryhorzhevskyi <kavo@Anatoliis-MacBook-Pro.local>
2025-07-11 11:14:08 -0400

    Добавлены последние изменения

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -7,7 +7,8 @@ const router = express.Router();
 
 // Настройка multer
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/certificates"),
+  destination: (req, file, cb) => cb(null, path.join(__dirname, "../public/uploads/certificates")),
+
   filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
 });
 const upload = multer({ storage });

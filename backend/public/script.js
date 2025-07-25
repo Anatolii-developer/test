946c0028b0a7436d55945fc02ebfb2aed36e017d
Anatolii Hryhorzhevskyi <kavo@Anatoliis-MacBook-Pro.local>
2025-07-25 10:44:34 -0400

    Добавлены последние изменения

--- backend/public/script.js
+++ backend/public/script.js
@@ -391,8 +391,6 @@ let users = [];
 let selectedParticipants = [];
 
 document.addEventListener("DOMContentLoaded", async () => {
-  if (window.location.pathname.includes("registration")) return;
-
   try {
     const res = await fetch(`${API_BASE}/api/users`);
     users = await res.json();
@@ -400,7 +398,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.error("Помилка завантаження користувачів:", err);
   }
 });
-
 
 function openUserModal() {
   

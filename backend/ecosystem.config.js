a9bb2cfb232a43129d605639930036c9e86ddbfa
Anatolii Hryhorzhevskyi <kavo@Anatoliis-MacBook-Pro.local>
2025-07-25 11:06:30 -0400

    Добавлены последние изменения

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "psychologist-api",
+      script: "server.js",
+      cwd: "./backend",
+      env: {
+        NODE_ENV: "production",
+        PORT: 5050,
+        MONGO_URI: "mongodb+srv://admin:admin2025@institution.hdquoso.mongodb.net/institution?retryWrites=true&w=majority&appName=institution"
+      }
+    }
+  ]
+};

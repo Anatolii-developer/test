eacdbea0dae520edb8129c46d994b0025a3d310a
Anatolii Hryhorzhevskyi <kavo@Anatoliis-MacBook-Pro.local>
2025-07-25 10:54:07 -0400

    Добавлены последние изменения

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,10 @@ app.use("/api/roles", roleRoutes);
 
 
 // ✅ MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
 

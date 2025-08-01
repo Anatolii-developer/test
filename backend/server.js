31c47af5834a1a74a17ed14982c7347e1e4b4e14
Anatolii Hryhorzhevskyi <kavo@Anatoliis-MacBook-Pro.local>
2025-08-01 09:51:10 -0400

    Добавлены последние изменения

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+
 // ✅ Роуты
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
